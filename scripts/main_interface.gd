337d245c8939007c5639caff2498d5072c5f6e42
IdfbAn <lukazaridze4@gmail.com>
2026-09-09 14:32:33 +0400

    Optimize UI name and age updates and remove trailing whitespace

--- scripts/main_interface.gd
+++ scripts/main_interface.gd
@@ -5,7 +5,25 @@ var game
 func _ready():
     game = get_parent()
 
+    await game.player_person_created
+    update_names_ages()
+
 func _process(_delta):
+    pass
+
+func _on_age_up_pressed():
+    for person: Person in game.all_people:
+        if person.person_alive:
+            person.person_age += 1
+
+    update_names_ages()
+
+    %Events.text = (
+        "You are now %d years old!\n" % Player.person_controlled.person_age
+        + %Events.text
+    )
+
+func update_names_ages():
     $You.text = "%s, age %d" % [Player.person_controlled.person_name,
         Player.person_controlled.person_age]
     $Parents.text = "Mother: %s, age %d\nFather: %s, age %d" % [
@@ -14,13 +32,3 @@ func _process(_delta):
         Player.person_controlled.father.person_name,
         Player.person_controlled.father.person_age
     ]
-
-func _on_age_up_pressed():
-    for person: Person in game.all_people:
-        if person.person_alive:
-            person.person_age += 1
-            
-    %Events.text = (
-        "You are now %d years old!\n" % Player.person_controlled.person_age
-        + %Events.text
-    )
