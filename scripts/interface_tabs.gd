7ca9940f2b238c1731a884c21627783446e6ff3a
IdfbAn <lukazaridze4@gmail.com>
2026-09-13 23:12:02 +0400

    Remove family tree

--- scripts/interface_tabs.gd
+++ scripts/interface_tabs.gd
@@ -1,12 +1,9 @@
 extends Control
 
-@onready var interfaces: Array[Control] = [$"../MainInterface", $"../FamilyTree"]
+@onready var interfaces: Array[Control] = [$"../MainInterface"]
 
 func _on_main_pressed():
     switch_interface($"../MainInterface")
-
-func _on_family_tree_pressed():
-    switch_interface($"../FamilyTree")
 
 func switch_interface(scene: Control):
     assert(scene in interfaces, "Given scene not in interfaces array")
